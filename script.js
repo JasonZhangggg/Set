771549a30d813cd4b2b8f55f90cf5a0adf5d8234
Jason Zhang <mailtojason.zhang@gmail.com>
2024-07-14 16:46:18 -0700

    added solutions tab

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const colors = ["red", "purple", "green"];
 const shapes = ["squiggle", "diamond", "oval"];
 const shadings = ["solid", "striped", "empty"];
 
+const grid = document.querySelector(".grid");
+const submit = document.querySelector("#submit");
+const deselect = document.querySelector("#deselect");
+const restart = document.querySelector("#restart");
+const helpButton = document.querySelector(".help");
+const helpMenu = document.querySelector(".help-menu");
+const solutionMenu = document.querySelector(".solution-menu");
+const solutionButton = document.querySelector(".solution");
+const exampleGrid = document.querySelector("#example-grid");
+const solutionGrid = document.querySelector("#solution-grid");
+const closeHelp = document.querySelector("#close-help");
+const closeSolution = document.querySelector("#close-sol");
+const popup = document.querySelector(".popup");
+const remaining = document.querySelector("#remaining");
+
 class Deck {
   constructor() {
     this._numCards = 81;
@@ -80,37 +95,38 @@ class Card {
 
 let board = [];
 let selected = [];
+let everyPosSet = [];
 
 function initBoard() {
-  board = [];
-  selected = [];
-  let deck = new Deck();
-  for (let i = 0; i < 12; i++) {
-    board.push(deck.drawCard());
-  }
+  do {
+    board = [];
+    selected = [];
+    let deck = new Deck();
+    for (let i = 0; i < 12; i++) {
+      board.push(deck.drawCard());
+    }
+    everyPosSet = everySetOnBoard(board);
+  } while (everyPosSet.length === 0);
+  updateRemaining();
 
   board.forEach((card) => {
     let cardElem = document.createElement("button");
     cardElem.addEventListener("click", () => {
-      cardElem.classList.toggle("selected");
-      card.selected = !card.selected;
-      if (card.selected) {
-        selected.push(card);
-      } else {
-        selected = selected.filter((c) => c !== card);
-      }
-      console.log(selected.length);
-      if (selected.length === 3) {
-        submit.className = "black-button";
-      } else {
-        submit.className = "gray-button";
+      if (card.selected || selected.length < 3) {
+        card.selected = !card.selected;
+        cardElem.classList.toggle("selected");
+        selected = card.selected
+          ? selected.concat(card)
+          : selected.filter((c) => c !== card);
       }
 
-      if (selected.length === 0) {
-        deselect.className = "gray-button";
-      } else {
-        deselect.className = "white-button";
-      }
+      selected.length === 3
+        ? (submit.className = "black-button")
+        : (submit.className = "gray-button");
+
+      selected.length === 0
+        ? (deselect.className = "gray-button")
+        : (deselect.className = "white-button");
     });
 
     cardElem.classList.add("card");
@@ -120,6 +136,14 @@ function initBoard() {
   });
 }
 
+function updateRemaining() {
+  remaining.innerHTML = "";
+  for (let i = 0; i < everyPosSet.length; i++) {
+    let span = document.createElement("span");
+    span.classList.add("circle");
+    remaining.appendChild(span);
+  }
+}
 function validSet(card1, card2, card3) {
   let setAttributes = ["color", "shape", "number", "shading"];
   for (let attribute of setAttributes) {
@@ -139,15 +163,40 @@ function validSet(card1, card2, card3) {
   return true;
 }
 
+function everySetOnBoard(board) {
+  let everyPosSet = [];
+  //triple nested loop to see all possible cards and if they work as sets
+  for (let a = 0; a < board.length - 2; a++) {
+    for (let b = a + 1; b < board.length - 1; b++) {
+      for (let c = b + 1; c < board.length; c++) {
+        //check if valid set, and if it is then we add to our big hashset
+        let currentSet = [board[a], board[b], board[c]];
+        if (validSet(board[a], board[b], board[c])) {
+          everyPosSet.push(currentSet);
+        }
+      }
+    }
+  }
+  return everyPosSet;
+}
+
 function handleSetClick(selected) {
   //the event handler when a player clicks a set and goes through the
   //process of verifying it as a set
   //verifies its a set and initiates the replacing process to start
-  if (validSet(selected[0], selected[1], selected[2])) {
+  if (everyPosSet.some((set) => set.every((card) => selected.includes(card)))) {
     showPopup("You found a set!");
+    everyPosSet = everyPosSet.filter(
+      (set) => !set.every((card) => selected.includes(card))
+    );
+    updateRemaining();
     deselectAll();
   } else {
-    showPopup("Not a set");
+    if (validSet(selected[0], selected[1], selected[2])) {
+      showPopup("Already found");
+    } else {
+      showPopup("Not a set");
+    }
   }
 }
 
@@ -171,16 +220,6 @@ function showPopup(message) {
   }, 2000);
 }
 
-const grid = document.querySelector(".grid");
-const submit = document.querySelector("#submit");
-const deselect = document.querySelector("#deselect");
-const restart = document.querySelector("#restart");
-const helpButton = document.querySelector(".help");
-const helpMenu = document.querySelector("dialog");
-const setExample = document.querySelector(".set-example");
-const closeButton = document.querySelector(".close");
-const popup = document.querySelector(".popup");
-
 // Generate example graphics
 example = [
   new Card("green", "squiggle", 1, "empty"),
@@ -195,11 +234,28 @@ example.forEach((card) => {
   let cardElem = document.createElement("button");
   cardElem.classList.add("card");
   cardElem.appendChild(card.createIcon());
-  setExample.appendChild(cardElem);
+  exampleGrid.appendChild(cardElem);
+});
+
+solutionButton.addEventListener("click", () => {
+  solutionMenu.showModal();
+  solutionGrid.innerHTML = "";
+  everyPosSet.forEach((set) => {
+    set.forEach((card) => {
+      let cardElem = document.createElement("button");
+      cardElem.classList.add("card");
+      cardElem.appendChild(card.createIcon());
+      solutionGrid.appendChild(cardElem);
+    });
+  });
+});
+
+closeSolution.addEventListener("click", () => {
+  solutionMenu.close();
 });
 
 //Event listeners for the help menu
-closeButton.addEventListener("click", () => {
+closeHelp.addEventListener("click", () => {
   helpMenu.close();
 });
 
